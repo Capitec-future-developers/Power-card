14067823798fbaa257ab120e6a09c50bc8201f86
Omphile Mohlala <omphilestudent@gmail.com>
2025-08-22 10:12:23 +0200

    added new stuff

--- js/header.js
+++ js/header.js
@@ -1,21 +1,23 @@
 const header = document.querySelector('.header');
 
 header.innerHTML = `
-<div class="logo">
-      <img src="./img/Logo.png" alt="logo" style="position: absolute; height: 30px;">
-      <span class="material-icons-sharp" style="position: absolute; left: 220px; color: red; cursor: pointer;">menu</span>
-      <div class="right-side">
-        <span class="material-icons-sharp">notifications</span>
-        <span class="material-icons-sharp">history</span>
-        <span class="material-icons-sharp">star</span>
-        <img src="./img/issuer.png" alt="issuer" style="width: 30px; height: 30px; border-radius: 50%; border: 2px dashed #146;">
-        <img src="./img/switch.png" alt="switch" style="width: 30px; height: 30px; border-radius: 50%;">
-        <div class="profile">
-          <span class="material-icons-sharp">person</span>
-          <span class="name">Omphile Mohlala</span>
-          <span class="material-icons-sharp">arrow_drop_down</span>
-        </div>
+  <div class="header-bar">
+    <div class="logo">
+      <img src="./img/Logo.png" alt="logo" class="logo-img">
+      <span class="material-icons-sharp menu-btn">menu</span>
+    </div>
+    <div class="right-side">
+      <span class="material-icons-sharp">notifications</span>
+      <span class="material-icons-sharp">history</span>
+      <span class="material-icons-sharp">star</span>
+      <img src="./img/issuer.png" alt="issuer" class="issuer-icon">
+      <img src="./img/switch.png" alt="switch" class="switch-icon">
+      <div class="profile">
+        <span class="material-icons-sharp">person</span>
+        <span class="name">Omphile Mohlala</span>
+        <span class="material-icons-sharp">arrow_drop_down</span>
       </div>
-      </div>
-    <div class="subheader" id="subheader">Hi OmphileMohlala, Welcome to PowerCARD 3.5 !</div>
+    </div>
+  </div>
+  <div class="subheader" id="subheader">Hi OmphileMohlala, Welcome to PowerCARD 3.5 !</div>
 `;
